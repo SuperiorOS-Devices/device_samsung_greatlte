c03afa8107c732d8d5af07ee5c0da63831ad7a57
HaSSaN-L <64253455+HaSSaN-L@users.noreply.github.com>
2020-07-13 23:45:25 -0700

    greatlte: SuperiorOS bringup

--- superior_greatlte.mk
+++ superior_greatlte.mk
@@ -22,12 +22,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/greatlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := greatlte
-PRODUCT_NAME := lineage_greatlte
+PRODUCT_NAME := superior_greatlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N950F
 PRODUCT_MANUFACTURER := samsung
